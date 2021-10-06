b5da7a6564667fbc31b8daab9e9f85e37f167417
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-10-06 11:37:54 +0900

    메인 페이지 (Introduction) 제작

---
+++ using-react/src/components/Introduction/Introduction.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const IntroductionContent = styled.div`
+  height: 100vh;
+  background: rgb(22, 230, 220);
+  background: linear-gradient(
+    83deg,
+    rgba(22, 230, 220, 1) 32%,
+    rgba(14, 125, 233, 1) 75%
+  );
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 32px;
+`;
+const Introduction = () => {
+  const [describe, setDescribe] = useState(1);
+  const [describeText, setDescibeText] = useState("꿈꾸는");
+
+  const changeDescribe = () => {
+    switch (describe) {
+      case 1:
+        setDescibeText("노력하는");
+        setDescribe(2);
+        break;
+      case 2:
+        setDescibeText("즐기는");
+        setDescribe(3);
+        break;
+      case 3:
+        setDescibeText("발전하는");
+        setDescribe(4);
+        break;
+      case 4:
+        setDescibeText("꿈꾸는");
+        setDescribe(1);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const repeatDescribe = () => {
+    setTimeout(changeDescribe, 5000);
+  };
+
+  useEffect(() => {
+    repeatDescribe();
+  }, [repeatDescribe]);
+  return (
+    <IntroductionContent>
+      <h1>
+        안녕하세요! <br />
+        {describeText} 개발자 이수동입니다.
+      </h1>
+    </IntroductionContent>
+  );
+};
+
+export default Introduction;
---
+++ using-react/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Introduction from "../../components/Introduction/Introduction";
+
+const MainPage = () => {
+  return <Introduction />;
+};
+
+export default MainPage;
