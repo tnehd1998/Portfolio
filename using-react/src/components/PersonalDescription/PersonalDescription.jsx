ff5fe651c208d8fb1c9878299c89505dc7141595
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-10-09 09:48:42 +0900

    TechStackItem (Progress Bar 제작)

--- using-react/src/components/PersonalDescription/PersonalDescription.jsx
+++ using-react/src/components/PersonalDescription/PersonalDescription.jsx
@@ -83,37 +83,60 @@ const PersonalDescription = () => {
       <TechStackContainer>
         <TechStackTitle>Frontend</TechStackTitle>
         <TechStackItems>
-          <TechStackItem name={"HTML5"} imageUrl={"tech-stack-img/html5.png"} />
-          <TechStackItem name={"CSS3"} imageUrl={"tech-stack-img/css3.png"} />
+          <TechStackItem
+            name={"HTML5"}
+            imageUrl={"tech-stack-img/html5.png"}
+            level={85}
+          />
+          <TechStackItem
+            name={"CSS3"}
+            imageUrl={"tech-stack-img/css3.png"}
+            level={80}
+          />
           <TechStackItem
             name={"SASS/SCSS"}
             imageUrl={"tech-stack-img/sass.png"}
+            level={70}
           />
           <TechStackItem
             name={"styled-components"}
             imageUrl={"tech-stack-img/styled-components.png"}
+            level={60}
           />
           <TechStackItem
             name={"JavaScript"}
             imageUrl={"tech-stack-img/javascript.png"}
+            level={75}
           />
           <TechStackItem
             name={"TypeScript"}
             imageUrl={"tech-stack-img/typescript.png"}
+            level={50}
           />
-          <TechStackItem name={"React"} imageUrl={"tech-stack-img/react.png"} />
-          <TechStackItem name={"Redux"} imageUrl={"tech-stack-img/redux.png"} />
+          <TechStackItem
+            name={"React"}
+            imageUrl={"tech-stack-img/react.png"}
+            level={60}
+          />
+          <TechStackItem
+            name={"Redux"}
+            imageUrl={"tech-stack-img/redux.png"}
+            level={25}
+          />
           <TechStackItem
             name={"Redux-Saga"}
             imageUrl={"tech-stack-img/redux-saga.png"}
+            level={15}
           />
           <TechStackItem
             name={"React Hooks"}
             imageUrl={"tech-stack-img/react-hooks.png"}
+            level={50}
           />
           <TechStackItem
             name={"GraphQL"}
             imageUrl={"tech-stack-img/graphql.png"}
+            level={15}
           />
         </TechStackItems>
         <TechStackTitle>Backend</TechStackTitle>
@@ -121,34 +144,45 @@ const PersonalDescription = () => {
           <TechStackItem
             name={"NodeJS"}
             imageUrl={"tech-stack-img/nodejs.png"}
+            level={35}
           />
           <TechStackItem
             name={"Express"}
             imageUrl={"tech-stack-img/express.png"}
+            level={25}
           />
           <TechStackItem
             name={"PostgreSQL"}
             imageUrl={"tech-stack-img/postgresql.png"}
+            level={45}
           />
           <TechStackItem
             name={"Oracle"}
             imageUrl={"tech-stack-img/oracle.png"}
+            level={45}
           />
           <TechStackItem
             name={"MongoDB"}
             imageUrl={"tech-stack-img/mongodb.png"}
+            level={35}
           />
         </TechStackItems>
         <TechStackTitle>Developer Tools</TechStackTitle>
         <TechStackItems>
-          <TechStackItem name={"Git"} imageUrl={"tech-stack-img/git.png"} />
+          <TechStackItem
+            name={"Git"}
+            imageUrl={"tech-stack-img/git.png"}
+            level={65}
+          />
           <TechStackItem
             name={"Github"}
             imageUrl={"tech-stack-img/github.png"}
+            level={60}
           />
           <TechStackItem
             name={"Firebase"}
             imageUrl={"tech-stack-img/firebase.png"}
+            level={45}
           />
         </TechStackItems>
       </TechStackContainer>
--- using-react/src/components/TechStackItem/TechStackItem.jsx
+++ using-react/src/components/TechStackItem/TechStackItem.jsx
@@ -11,14 +11,40 @@ const Content = styled.li`
 const LogoImage = styled.img`
   width: 6rem;
   height: 6rem;
-  margin-bottom: 2rem;
 `;
 
-const TechStackItem = ({ name, imageUrl }) => {
+const TechStackItemTitle = styled.p`
+  font-size: 16px;
+  margin: 2rem 0;
+`;
+
+const TechStackItemLevel = styled.p`
+  font-size: 12px;
+  margin-top: 1rem;
+`;
+
+const SkillBar = styled.div`
+  width: 5rem;
+  height: 5px;
+  background-color: white;
+`;
+
+const SkillBarRange = styled.div`
+  width: ${(props) => `${props.level}%`};
+  height: 5px;
+  background-color: ${(props) =>
+    props.level > 70 ? "green" : props.level > 40 ? "yellow" : "red"};
+`;
+
+const TechStackItem = ({ name, imageUrl, level }) => {
   return (
     <Content>
       <LogoImage src={imageUrl} />
-      <h1>{name}</h1>
+      <TechStackItemTitle>{name}</TechStackItemTitle>
+      <SkillBar>
+        <SkillBarRange level={level} />
+      </SkillBar>
+      <TechStackItemLevel>숙련도 : {level}/100</TechStackItemLevel>
     </Content>
   );
 };
