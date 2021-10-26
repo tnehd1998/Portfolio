6bf08e55efdc2fc1e47020548d6fb5028a222714
tnehd1998 <72953316+tnehd1998@users.noreply.github.com>
2021-10-26 12:24:38 +0900

    프로젝트 내용 수정 및 추가

--- src/components/ProjectInfo/ProjectInfo.jsx
+++ src/components/ProjectInfo/ProjectInfo.jsx
@@ -6,6 +6,7 @@ import {
   ProjectTitle,
 } from "./ProjectInfo.styles.jsx";
 import Youtube from "../../assets/images/project-item-img/youtubeimg.png";
+import Portfolio from "../../assets/images/project-item-img/portfolioimg.png";
 
 import ProjectItem from "../../components/ProjectItem/ProjectItem";
 
@@ -23,7 +24,18 @@ const ProjectInfo = () => {
           "자주 사용하는 YouTube의 기능을 그대로 따라서 만들어본 프로젝트",
         ]}
         projectLink={"https://github.com/tnehd1998/Youtube"}
-        hashtags={["첫 프로젝트", "YouTube", "개인", "재미", "클론"]}
+        hashtags={["YouTube", "개인", "재미", "클론"]}
+      />
+      <ProjectItem
+        imageUrl={Portfolio}
+        title={"개인 포트폴리오"}
+        period={"21.10.06 ~ 현재 진행중"}
+        descriptions={[
+          "직접 만들어본 자기 소개용 첫 프로젝트",
+          "자기소개, 사용기술, 프로젝트를 설명하는 사이트",
+        ]}
+        projectLink={"https://github.com/tnehd1998/Portfolio"}
+        hashtags={["첫 프로젝트", "자기소개", "포트폴리오"]}
       />
     </ProjectPageContainer>
   );
