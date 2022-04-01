36a90b353d90c3c154aa4401f63b8de2d024fc37
이수동 <tnehd1998@gmail.com>
2022-04-01 12:09:03 +0900

    feat: Blog 데이터 추가

--- src/store/blog.ts
+++ src/store/blog.ts
@@ -22,5 +22,12 @@ export const blogState = atom<IBlog[]>({
       title: "[B1] 상태관리, 이제 Recoil 하세요.",
       question: "Redux 말고 React스럽게 상태관리를 할 수는 없을까?",
     },
+    {
+      blogId: 4,
+      blogLink:
+        "https://velog.io/@tnehd1998/%ED%95%A8%EA%BB%98-%EC%9E%90%EB%9D%BC%EA%B8%B0-%EC%95%A0%EC%9E%90%EC%9D%BC%EB%A1%9C-%EA%B0%80%EB%8A%94-%EA%B8%B8",
+      title: "함께 자라기 | 애자일로 가는 길",
+      question: "개발자로써 어떻게 하면 제대로 성장할 수 있을까?",
+    },
   ],
 });
